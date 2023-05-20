20f09ad523b246cb1872013242761fc387c96a6b
Mohammad <mo.movahedinia@gmail.com>
2023-05-21 04:28:18 +0430

    fearAndGreed

---
+++ src/components/fearAndGreed.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
+
+function getData() {
+  return [
+    ["Label", "Value"],
+    [" ", 52],
+  ];
+}
+
+const options = {
+  width: 400,
+  height: 400,
+  redFrom: 80,
+  redTo: 100,
+  yellowFrom: 60,
+  yellowTo: 80,
+  minorTicks: 5,
+};
+
+function FearAndGreed() {
+  const [data, setData] = useState(getData);
+
+//   useEffect(() => {
+//     const id = setInterval(() => {
+//       setData(getData());
+//     }, 3000);
+
+//     return () => {
+//       clearInterval(id);
+//     };
+//   });
+
+  return (
+    <Chart
+      chartType="Gauge"
+      width='400px'
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
+
+export default FearAndGreed;
--- src/components/firstPage.jsx
+++ src/components/firstPage.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import Slider from "./slider";
 import "react-slideshow-image/dist/styles.css";
 import Footer from "./footer";
+import FearAndGreed from "./fearAndGreed";
 
 function firstPage() {
   return (
     <div>
       <div
-        style={{
-          // maxWidth: "80%",
-          // margin: "0",
-          // top: "0%",
-          // left: "50%",
-          // right: "50%",
+        style={{         
+          maxWidth: "80%",
+          margin: '0 auto'
         }}
       >
         <Slider />
         <div className="parts">
-          <div style={{background:'orange', color:'white'}}>بورس</div>
+          <h3>شاخص ترس و طمع بازار بورس</h3>
+          <FearAndGreed/>
         </div>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
