4311de49f494bc482c4bb8af15c898570c92633e
Mohammad <mo.movahedinia@gmail.com>
2023-06-13 14:53:26 +0430

    bubble

---
+++ src/components/bubble.jsx
@@ -0,0 +1,61 @@
+import { React, useState } from "react";
+import { useForm } from "react-hook-form";
+
+function Bubble() {
+  const [bubblePercentage, setBubblePercentage] = useState(0);
+  const [type, setType] = useState(1);
+  const [dollar, setDollar] = useState();
+  const [gold, setGold] = useState();
+  const [sekke, setSekke] = useState();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors } ,
+  // } = useForm();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(6);
+    // let result = (data.type * 0.9 * data.dollar * data.gold) / 31.1 + 50000;
+    let result = (type * 0.9 * dollar * gold) / 31.1 + 50000;
+    setBubblePercentage((sekke - result) / result);
+    console.log(bubblePercentage);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select value={type} onChange={(event) => setType(event.target.value)}>
+          <option value="1">سکه گرمی</option>
+          <option value="2.033">ربع سکه</option>
+          <option value="4.066">نیم سکه</option>
+          <option value="8.133">تمام سکه</option>
+        </select>
+        <label>
+          قیمت دلار:
+          <input type="number" value={dollar} onChange={(event) => setDollar(event.target.value)} required/>
+          {/* {errors.dollar && <span>This field is required</span>} */}
+        </label>
+        <label>
+          قیمت انس طلای جهانی:
+          <input type="number" value={gold} onChange={(event) => setGold(event.target.value)} required />
+          {/* {errors.gold && <span>This field is required</span>} */}
+        </label>
+        <label>
+          قیمت روز سکه:
+          <input
+            type="number"
+            value={sekke} onChange={(event) => setSekke(event.target.value)}
+            // ref={register({ required: true })}
+          />
+          {/* {errors.sekke && <span>This field is required</span>} */}
+        </label>
+
+        <input type="submit" value="محاسبه"/>
+      </form>
+      <p>{bubblePercentage}</p>
+    </div>
+  );
+}
+
+export default Bubble;
--- src/components/firstPage.jsx
+++ src/components/firstPage.jsx
@@ -3,6 +3,7 @@ import Slider from "./slider";
 import "react-slideshow-image/dist/styles.css";
 import Footer from "./footer";
 import FearAndGreed from "./fearAndGreed";
+import Bubble from "./bubble";
 
 function firstPage() {
   return (
@@ -17,6 +18,10 @@ function firstPage() {
         <div className="parts">
           <h3>شاخص ترس و طمع بازار بورس</h3>
           <FearAndGreed/>
+        </div>
+        <div className="parts">
+          <h3>حباب سکه</h3>
+          <Bubble/>
         </div>
       </div>
       <Footer/>
