9ea2b870e402be507441d8435638c0bfd54e6ea9
Mohammad <mo.movahedinia@gmail.com>
2023-10-04 04:06:28 +0330

    board calendar

--- src/components/dashboard/board.jsx
+++ src/components/dashboard/board.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { data } from "./tabloData";
+import { data as dataStatic } from "./tabloData";
 import axios from "axios";
+// import DatePicker from 'react-datepicker'
+// import 'react-datepicker/dist/react-datepicker.css'
+import moment from "jalali-moment";
+import { DatePicker } from "zaman";
+// import { DatePicker } from "@kasraghoreyshi/datepicker";
+// import "@kasraghoreyshi/calendar/styles.css";
+// import "@kasraghoreyshi/datepicker/styles.css";
 
 function Tablo() {
-  const filteredData = Object.entries(data).filter(([key]) => !/\d/.test(key));
+  const filteredData = Object.entries(dataStatic).filter(
+    ([key]) => !/\d/.test(key)
+  );
   filteredData.sort(([, value1], [, value2]) => value2.sum - value1.sum);
   const sortedData = Object.fromEntries(filteredData);
 
   const [stocks, setStocks] = useState(sortedData);
 
   const [search, setSearch] = useState();
+
   function handleSearch(event) {
     event.preventDefault();
+
     if (!search) {
       setStocks(sortedData);
     } else {
@@ -26,17 +37,41 @@ function Tablo() {
   }
 
   useEffect(() => {
-    console.log(data);
+    console.log(dataStatic);
+    const boardDate = moment(new Date()).format("jYYYY-jMM-jDD");
+    axios
+      .get("https://api-machinetrading.onrender.com/api/table")
+      .then(function (response) {
+        // setFacultyMenu(response.data);
+      })
+      .catch(function () {
+        // NotificationManager.warning("بارگیری دانشکده‌ها با خطا مواجه شد");
+      });
   }, []);
 
   return (
     <div>
+      {/* <DatePicker
+      selected={selected}
+      onChange={handle}
+      dateFormat="jYYYY/jMM/jDD"
+      calendarClassName="jalali-datepicker"
+      adapter={moment}
+      /> */}
+      {/* <DatePicker onChange={(event) =>console.log(event.value)}/> */}
+      <DatePicker
+        onChange={(event) =>
+          console.log(moment(event.value).format("jYYYY-jMM-jDD"))
+        }
+        defaultValue={new Date()}
+      />
+      {/* <DatePicker onChange={(event) =>console.log(event.value)} autoUpdate={true}/> */}
       <p>تاریخ امتیازدهی: 1402/05/31</p>
       <form
         // className="form-inline my-2 my-lg-0"
         // id="searchForm"
         onSubmit={handleSearch}
-        style={{ display: "inline"}}
+        style={{ display: "inline" }}
       >
         <input
           // className="form-control mr-sm-2"
@@ -46,7 +81,7 @@ function Tablo() {
           style={{
             borderRadius: "7px",
             display: "inline",
-             margin:'15px'
+            margin: "15px",
           }}
           placeholder="جستجوی نام سهم"
         />
