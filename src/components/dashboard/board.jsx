4276986383f75d091f0ee216d0b9f0f1ee2107fe
Immohammad <mo.movahedinia@gmail.com>
2023-10-18 22:45:24 +0330

    board done partly

--- src/components/dashboard/board.jsx
+++ src/components/dashboard/board.jsx
@@ -1,53 +1,99 @@
 import React, { useEffect, useState } from "react";
 import { data as dataStatic } from "./tabloData";
 import axios from "axios";
-// import DatePicker from 'react-datepicker'
-// import 'react-datepicker/dist/react-datepicker.css'
 import moment from "jalali-moment";
 import { DatePicker } from "zaman";
+import loading from "../assets/loading.gif";
+// import DatePicker from 'react-datepicker'
+// import 'react-datepicker/dist/react-datepicker.css'
 // import { DatePicker } from "@kasraghoreyshi/datepicker";
 // import "@kasraghoreyshi/calendar/styles.css";
 // import "@kasraghoreyshi/datepicker/styles.css";
 
+
 function Tablo() {
-  const filteredData = Object.entries(dataStatic).filter(
-    ([key]) => !/\d/.test(key)
-  );
-  filteredData.sort(([, value1], [, value2]) => value2.sum - value1.sum);
-  const sortedData = Object.fromEntries(filteredData);
+  // const filteredData = Object.entries(dataStatic).filter(
+  //   ([key]) => !/\d/.test(key)
+  // );
+  // filteredData.sort(([, value1], [, value2]) => value2.sum - value1.sum);
+  // const sortedData = Object.fromEntries(filteredData);
 
-  const [stocks, setStocks] = useState(sortedData);
+  const [stocks, setStocks] = useState();
+  // let wholeBoard;
+  const [wholeBoard, setWholeBoard] = useState(null);
 
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState('');
+  const [requestedDate, setRequestedDate] = useState(moment(new Date()).subtract(3, 'month').format("jYYYY-jMM-jDD"));
+
+  useEffect(() => {
+    // console.log(dataStatic);
+    const boardDate = moment(new Date()).subtract(3, 'month').format("jYYYY-jMM-jDD");
+    axios
+      .get(`http://45.129.36.165:3000/api/board/getAll?date=${boardDate}`, {
+        headers: {
+          authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTc2NDY4MjF9.LjWeNrOnzlZ6qZefM91kgjtzz_hKc4KIrK9RW3y6TcQ",
+        }
+      })
+      .then((response) => {
+        // const sorted = response.data.slice().sort((a, b) => b.sum - a.sum);
+        // wholeBoard = response.data
+        setWholeBoard(response.data)
+        // console.log(wholeBoard.find(item => item.stockTitle === "سکارون"))
+        setStocks(response.data)
+        console.log(response.data)
+        console.log(boardDate)
+      })
+      .catch((error) => {
+        console.log("xz")
+        console.log(boardDate)
+        // NotificationManager.warning("بارگیری دانشکده‌ها با خطا مواجه شد");
+      });
+  }, []);
 
   function handleSearch(event) {
     event.preventDefault();
 
     if (!search) {
-      setStocks(sortedData);
+      setStocks(wholeBoard);
+      // console.log(wholeBoard)
     } else {
+      // console.log('no')
+      // console.log(foundItems)
+      const foundItems = wholeBoard.filter(item => item.stockTitle.includes(search));
       setStocks(
-        Object.fromEntries(
-          Object.entries(sortedData).filter(([key, value]) =>
-            key.includes(search)
-          )
-        )
+        foundItems
       );
     }
   }
+  // setStocks(
+  //   Object.fromEntries(
+  //     Object.entries(sortedData).filter(([key, value]) =>
+  //       key.includes(search)
+  //     )
+  //   )
+  // );
 
-  useEffect(() => {
-    console.log(dataStatic);
-    const boardDate = moment(new Date()).format("jYYYY-jMM-jDD");
+  function handleDate(event) {
+    // event.preventDefault();
+    setRequestedDate(moment(event.value).subtract(3, 'month').format("jYYYY-jMM-jDD"))
     axios
-      .get("https://api-machinetrading.onrender.com/api/table")
-      .then(function (response) {
-        // setFacultyMenu(response.data);
+      .get(`http://45.129.36.165:3000/api/board/getAll?date=${requestedDate}`, {
+        headers: {
+          authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTc2NDY4MjF9.LjWeNrOnzlZ6qZefM91kgjtzz_hKc4KIrK9RW3y6TcQ",
+        }
       })
-      .catch(function () {
+      .then((response) => {
+        setWholeBoard(response.data)
+        setStocks(response.data)
+        console.log(response.data)
+        console.log(requestedDate)
+      })
+      .catch((error) => {
+        console.log("xz")
+        console.log(requestedDate)
         // NotificationManager.warning("بارگیری دانشکده‌ها با خطا مواجه شد");
       });
-  }, []);
+  }
 
   return (
     <div>
@@ -60,13 +106,15 @@ function Tablo() {
       /> */}
       {/* <DatePicker onChange={(event) =>console.log(event.value)}/> */}
       <DatePicker
-        onChange={(event) =>
+        onChange={(event) => {
           console.log(moment(event.value).format("jYYYY-jMM-jDD"))
+          handleDate(event)
+        }
         }
         defaultValue={new Date()}
       />
       {/* <DatePicker onChange={(event) =>console.log(event.value)} autoUpdate={true}/> */}
-      <p>تاریخ امتیازدهی: 1402/05/31</p>
+      <p>تاریخ امتیازدهی: {requestedDate}</p>
       <form
         // className="form-inline my-2 my-lg-0"
         // id="searchForm"
@@ -77,6 +125,7 @@ function Tablo() {
           // className="form-control mr-sm-2"
           value={search}
           onChange={(event) => setSearch(event.target.value)}
+          // onChange={handleSearch}
           type="search"
           style={{
             borderRadius: "7px",
@@ -107,7 +156,46 @@ function Tablo() {
           </tr>
         </thead>
         <tbody>
-          {stocks &&
+
+          {stocks ?
+            (stocks.map((item, index) => (
+              <tr
+                key={index}
+                style={
+                  item.sum >= 75
+                    ? { backgroundColor: "#0B6623" }
+                    : item.sum >= 50
+                      ? { backgroundColor: "#8cc73c" }
+                      : item.sum >= 25
+                        ? { backgroundColor: "#87CEEB" }
+                        : item.sum >= -25
+                          ? { backgroundColor: "#808080" }
+                          : item.sum >= -50
+                            ? { backgroundColor: "#9b870c" }
+                            : item.sum >= -75
+                              ? { backgroundColor: "orange" }
+                              : { backgroundColor: "red" }
+                }
+              >
+                <td>{index + 1}</td>
+                <td>{item.stockTitle}</td>
+                <td>{item.suspicios_volume}</td>
+                <td>{item.intel_money}</td>
+                <td>{item.real_money}</td>
+                {/* <td>{value.final_last}</td> */}
+                {/* <td>{value.accumulation}</td> */}
+                <td>{item.buy_power}</td>
+                <td>{item.sum}</td>
+              </tr>
+            ))) : (
+              <tr>
+                <td colSpan={6}><img src={loading} className="loadingGif" /></td>
+              </tr>
+            )
+          }
+
+
+          {/* {stocks &&
             Object.entries(stocks).map(([key, value], index) => (
               <tr
                 key={key}
@@ -115,16 +203,16 @@ function Tablo() {
                   value.sum >= 75
                     ? { backgroundColor: "#0B6623" }
                     : value.sum >= 50
-                    ? { backgroundColor: "#8cc73c" }
-                    : value.sum >= 25
-                    ? { backgroundColor: "#87CEEB" }
-                    : value.sum >= -25
-                    ? { backgroundColor: "#808080" }
-                    : value.sum >= -50
-                    ? { backgroundColor: "#9b870c" }
-                    : value.sum >= -75
-                    ? { backgroundColor: "orange" }
-                    : { backgroundColor: "red" }
+                      ? { backgroundColor: "#8cc73c" }
+                      : value.sum >= 25
+                        ? { backgroundColor: "#87CEEB" }
+                        : value.sum >= -25
+                          ? { backgroundColor: "#808080" }
+                          : value.sum >= -50
+                            ? { backgroundColor: "#9b870c" }
+                            : value.sum >= -75
+                              ? { backgroundColor: "orange" }
+                              : { backgroundColor: "red" }
                 }
               >
                 <td>{index + 1}</td>
@@ -132,12 +220,11 @@ function Tablo() {
                 <td>{value.suspicios_volume}</td>
                 <td>{value.intel_money}</td>
                 <td>{value.real_money}</td>
-                {/* <td>{value.final_last}</td> */}
-                {/* <td>{value.accumulation}</td> */}
                 <td>{value.buy_power}</td>
                 <td>{value.sum}</td>
               </tr>
-            ))}
+            ))} */}
+
         </tbody>
       </table>
     </div>
